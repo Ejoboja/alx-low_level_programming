5b6472835be1694c5709abb50e0037af0f08ec7d
Tewan Ejoboja <ejobojat@gmail.com>
2023-10-20 00:39:41 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "list.h"
+/**
+* print_list - output all elements in a singly linked list
+*
+* @h: head of the list
+* Return: Total number of nodes in the list
+*/
+size_t print_list(const list_t *h)
+{
+		int i;
+
+		if (h == NULL)
+			return (0);
+
+		for (i = 1; h->next != NULL; i++)
+		{
+			if (h->str == NULL)
+				printf("[%u] %s\n", h->len, "(nil)");
+			else
+				printf("[%u] %s\n", h->len, h->str);
+			
+			h = h->next;
+		}
+		printf("[%u] %s\n", h->len, "(nil)");
+		return (i);
+}
